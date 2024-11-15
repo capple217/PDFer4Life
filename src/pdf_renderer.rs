28fec0ebd50bf9f86a66a7c18482b453101b7401
capple217 <575.perspective@gmail.com>
2024-11-15 16:19:15 -0600

    Curr functioning of renderer

--- src/pdf_renderer.rs
+++ src/pdf_renderer.rs
@@ -2,8 +2,30 @@ use dotenv::dotenv;
 use std::env;
 use pdfium_render::prelude::*;
 
-let pdfium = Pdfium::default();
+fn export_pdf_to_jpegs(path: &impl AsRef<Path>, password: Option<&str>) -> Result<(), PdfiumError> {
 
+    let pdfium = Pdfium::default();
+
+    let document = pdfium.load_pdf_from_file(path, password)?;
+
+    let render_config = PdfRenderConfig::new()
+        .set_target_width(2000)
+        .set_maximum_height(2000)
+        .rotate_if_landscape(PdfPageRenderRotation::Degrees90, true);
+
+    for (index, page) in document.pages().iter().enumerate() {
+        page.render_with_config(&render_config)?
+            .as_image()
+            .into_rgb8()
+            .save_with_format(
+                format!("test-page-{}.jpg", index),
+                image::ImageFormat::Jpeg
+            )
+            .map_err(|_| Pdfium::ImageError)?;
+    }
+
+    Ok(())
+}
 
 
 
